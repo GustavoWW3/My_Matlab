827682290d3e1a636856bd2e162ed6aa675163be
thesser1@gmail.com <thesser1@gmail.com>
2013-03-29 09:26:22 -0500

    removed files from main directory

--- dms2ddeg.m
+++
@@ -1,7 +0,0 @@
-function [decideg] = dms2ddeg(deg,min,sec)
-% converts degrees minutes seconds to decimal degrees
-% written 6/29/11 TJ Hesser
-
-decideg = deg + min./60 + sec./3600;
-
-end
--- unzip_files.m
+++
@@ -1,5 +0,0 @@
-files = dir('*.zip');
-for zz = 1:size(files,1);
-    filename = getfield(files(zz,1),'name');
-    unzip(filename);
-end
