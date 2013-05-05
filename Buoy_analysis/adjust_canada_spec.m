c6dfbbc42656b7cf1247bd5c16020e0128ac29bb
thesser1 <thesser1@gmail.com>
2013-05-05 07:29:59 -0500

    fixed GOM for production

--- Buoy_analysis/adjust_canada_spec.m
+++ Buoy_analysis/adjust_canada_spec.m
@@ -1,4 +1,14 @@
-function aa = adjust_canada_spec(aa)
+function aa = adjust_canada_spec(bb)
+
+ii = bb.hsb > 0.5;
+aa.date = bb.date(ii,:);
+aa.timemat = bb.timemat(ii,:);
+aa.ef = bb.ef(:,ii);
+aa.bw = bb.bw(:,ii);
+aa.freq = bb.freq(:,ii);
+aa.tm = bb.tm(ii);
+aa.hsb = bb.hsb(ii);
+aa.tpb = bb.tpb(ii);
 
 aa.efold = aa.ef;
 
@@ -8,10 +18,10 @@ nfreq = size(aa.freq,1);
 for jj = nfreq:-1:1
     dum = dum + aa.efold(jj,ii).*aa.bw(jj,ii);
     if dum == aa.hsb(ii).^2/16
-        aa.ef(1:jj,ii) = 0;
+        aa.ef(1:jj-1,ii) = 0;
         break
     elseif dum > aa.hsb(ii).^2/16
-        aa.ef(1:jj+1,ii) = 0;
+        aa.ef(1:jj,ii) = 0;
         break
     end
 end
@@ -26,9 +36,9 @@ if length(ip) > 1
         ip = ip(2);
     end
 end
-if ip < 12
-    ip = 12;
-end
+% if ip < 12
+%     ip = 12;
+% end
 
 aa.tp(ii) = 1.0/aa.freq(ip,ii);
 end
--- SWIMS/flume_eval.m
+++ SWIMS/flume_eval.m
@@ -2,10 +2,11 @@ function aa = flume_eval(fdir)
 
 ff = dir([fdir,'*.dat']);
 
-aa = read_swims_flume(fdir,ff(1).name);
+for zz = 1:size(ff,1)
+    aa = read_swims_flume(fdir,ff(zz).name);
 
-a = {'lo','id','hi'};
-d=[0.415 0.439 0.490];
+    a = {'lo','id','hi'};
+    d=[0.415 0.439 0.490];
 I=strcmp(a,aa.name(end-10:end-9));
 aa.h=d(I);
 
@@ -14,3 +15,4 @@ aa.dt = aa.time(2) - aa.time(1);
 aa = upcrossing(aa);
 aa = spec_analy(aa);
 aa = reflection(aa);
+end
